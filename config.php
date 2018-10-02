c0fe0ce860ffb5dcca417dcf103d445dc843223c
GlebFisher <43784335+GlebFisher@users.noreply.github.com>
2018-10-02 17:24:51 +0300

    Add files via upload

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+define('BOT_BASE_DIRECTORY', '/var/www');
+define('BOT_LOGS_DIRECTORY', BOT_BASE_DIRECTORY.'/logs');
+define('BOT_IMAGES_DIRECTORY', BOT_BASE_DIRECTORY.'/static');
+define('BOT_AUDIO_DIRECTORY', BOT_BASE_DIRECTORY.'/audio');
+
+define('CALLBACK_API_CONFIRMATION_TOKEN', '23004e86'); //Строка для подтверждения адреса сервера из настроек Callback API
+define('VK_API_ACCESS_TOKEN', '016cb55230eb83e34f620389s149345d975bc12e2fb6f30e0a833a51d345d0d123c3dc0abc1c864036sdf989fb8345'); //Ключ доступа сообщества
+define('YANDEX_API_KEY', 'efe8eb9b-a942-4143-90ad-1ff76f931521'); //Ключ для доступа к Yandex Speech Kit
